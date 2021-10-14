a1c6350a08d3093ab8ac33dc9ab15ce2a71be56c
Sergio Majé <smajefranco@gmail.com>
2021-10-14 04:23:45 -0500

    feat: authentication provider

--- src/app/auth/AuthProvider.jsx
+++ src/app/auth/AuthProvider.jsx
@@ -2,3 +2,31 @@ import { createContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const history = useHistory();
+
+    const [user, setUser] = useState(null, AuthContext);
+
+    const login = (userCredentials, fromLocation) => {
+        setUser(userCredentials);
+        fromLocation && fromLocation.push(fromLocation);
+    }
+
+    const logout = () => setUser(null);
+
+    const isLogged = () => !!user;
+
+    const contextValue = {
+        user,
+        isLogged,
+        login,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
