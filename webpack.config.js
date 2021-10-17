07083b9d092a3454daad325e0842c19d3acc1221
Sergio Majé <smajefranco@gmail.com>
2021-10-17 12:09:41 -0500

    chore: babel rules

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,32 @@ const path = require('path');
 const isProduction = (mode) => mode === 'production';
 const isDevelopment = (mode) => mode === 'development';
 
+// rules
+const rulesBabel = (mode) => ({
+    test: /\.jsx?$/i,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: [
+                '@babel/preset-env',
+                ['@babel/preset-react', {
+                    runtime: 'automatic',
+                    development: isDevelopment(mode)
+                }]
+            ]
+        }
+    }
+})
+
+const rulesCommon = (mode) => [
+    rulesBabel(mode)
+]
+
 // config
 const commonConfig = (mode) => ({
-    entry: path.join(__dirname, 'src', 'app')
+    entry: path.join(__dirname, 'src', 'app'),
+    module: { rules: rulesCommon(mode) }
 })
 
 const devConfig = {
