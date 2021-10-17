fc068287ea6d9871cd8d944a77d5debd9428273f
Sergio Majé <smajefranco@gmail.com>
2021-10-17 03:11:41 -0500

    feat(server): express server

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const { PORT } = require('./utils/config');
+
+const app = express();
+
+// MongoDB connection
+require('./database');
+
+// Settings
+app.set('port', PORT);
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+
+
+// Static files
+app.use('/', express.static(path.join(__dirname, '..', 'dist')));
+
+
+// Starting server
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+})
