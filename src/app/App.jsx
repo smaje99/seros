89714c07ce8ca227f46b5cf0dce3874a20ca8c49
Sergio Majé <smajefranco@gmail.com>
2021-10-17 18:13:28 -0500

    :art: feat: Web App struture

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import AppRouter from 'Routers/AppRouter';
+import { AuthProvider } from 'Auth/AuthProvider';
+
+const App = () => (
+    <>
+        <Router>
+            <AuthProvider>
+                <AppRouter />
+            </AuthProvider>
+        </Router>
+    </>
+);
+
+export default App;
--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 import ReactDom from "react-dom";
 
+import App from './App';
+
 ReactDom.render(
-    <h1>Index.js</h1>,
+    <App />,
     document.getElementById('app')
 )
