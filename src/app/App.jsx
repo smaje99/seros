4d87a93d205c92e3e729fd5e949d49fc27e2ef4f
Sergio Majé <smajefranco@gmail.com>
2021-10-18 11:22:42 -0500

    :art: App layout

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import AppRouter from 'Routers/AppRouter';
 import { AuthProvider } from 'Auth/AuthProvider';
+import Layout from 'Components/Layout';
 
 const App = () => (
     <>
         <Router>
             <AuthProvider>
-                <AppRouter />
+                <Layout>
+                    <AppRouter />
+                </Layout>
             </AuthProvider>
         </Router>
     </>
