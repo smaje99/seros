2f6fe8453610d33e6b5db7a3d99887ce9be442e1
Sergio Majé <smajefranco@gmail.com>
2022-01-18 23:01:32 -0500

    📝 Error handlers middlewares documentation

--- src/middleware/errorHandlers.js
+++ src/middleware/errorHandlers.js
@@ -2,6 +2,13 @@ const boom = require('@hapi/boom');
 
 const config = require('../utils/config');
 
+/**
+ * If the environment is development it returns the error
+ * with its stack, otherwise it only returns the error
+ * @param {*} error error
+ * @param {*} stack error stack
+ * @returns error depending on the environment
+ */
 function withErrorStack(error, stack) {
     return config.dev ? { ...error, stack } : error;
 }
@@ -12,24 +19,40 @@ function withErrorStack(error, stack) {
  * @param {*} err error
  * @param {*} req request
  * @param {*} res response
- * @param {*} next next middleware callback
+ * @param {function} next next middleware callback
  */
 function logErrors(err, req, res, next) {
     console.error(err);
     next(err);
 }
 
+/**
+ * Check if the error is wrapped, if not,
+ * it wraps it and returns a 500 status code
+ * @param {*} err error
+ * @param {*} req request
+ * @param {*} res response
+ * @param {function} next next middleware callback
+ */
 function wrapErrors(err, req, res, next) {
     next(err.isBoom ? err : boom.badImplementation(err));
 }
 
+/**
+ * Manages the error, returning the status code and
+ * the respective error message.
+ * @param {*} err error
+ * @param {*} req request
+ * @param {*} res response
+ * @param {function} next next middleware callback
+ */
 function errorHandler(err, req, res, next) {
     const {
         output: { statusCode, payload }
     } = err;
 
     res
-        .status(statusCode || 500)
+        .status(statusCode)
         .json(withErrorStack(payload, err.stack));
 }
 
