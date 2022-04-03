fab9cdd6c56a5c17cc2b639508aeaa00965ab182
Sergio Majé <smajefranco@gmail.com>
2022-04-03 01:23:23 -0500

    🚧 Spinner component and styles

---
+++ src/app/components/Preloaders/Spinner/Spinner.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function Spinner() {
+    return (
+        <div className="spinner">
+            <hr className="spinner__point"/>
+            <hr className="spinner__point"/>
+            <hr className="spinner__point"/>
+            <hr className="spinner__point"/>
+        </div>
+    )
+}
+
+export default Spinner
