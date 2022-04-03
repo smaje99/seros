89deeaf79f1dc6ef261732f498da36c80d880b33
Sergio Majé <smajefranco@gmail.com>
2022-04-03 17:39:27 -0500

    🎨 Add Layout structure

--- src/app/components/Layout/Layout.jsx
+++ src/app/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import Navigation from "Components/Navigation";
 
+import './style.css';
+
 const Layout = () => {
     return (
-        <>
+        <div className="layout">
             <Navigation />
             <Outlet />
-        </>
+        </div>
     )
 }
 
